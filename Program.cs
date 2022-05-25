8b5a9a894636887929da55aada0b7fec04498eb7
Peter Schären <peter.schaeren@swissteach.ch>
2022-05-25 09:07:55 +0200

    Initial version.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace iot.raspi.blinky
+{
+    class Program
+    {
+        static void Main(string[] margs)
+        {
+            Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+            int ledPin = 18;
+            using var controller = new GpioController();
+            controller.OpenPin(ledPin, PinMode.Output);
+            var loopCount = 1;
+            while (true)
+            { 
+                Console.WriteLine($"loop {loopCount++}");
+
+                controller.Write(ledPin, PinValue.High);
+                Thread.Sleep(200);
+                controller.Write(ledPin, PinValue.Low);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
